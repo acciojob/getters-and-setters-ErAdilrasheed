852643db230a0b793f7f5113238164548e727efe
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2025-05-20 16:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	let name;
+	let age;
+	Person(String name, Number age){
+		this.name=name;
+		this.age=age;
+	}
 
-class Student extends Person {}
+	get name(name){
+		return this.name;
+	}
+	get age(age){
+		return this.age;
+	}
+	set name(newName){
+		this.name = newName;
+	}
+	set age(newAge){
+		this.age = newAge;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(name + " is studying");
+	}
+}
+
+class Teacher extends Person {
+	function teach(){
+		console.log(name + " is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
